cdb85c4b62885e8b0ff43ea085b4d5d386ac6b6c
nandu890 <93024230+likhitha1186@users.noreply.github.com>
2025-03-13 19:14:25 +0530

    minor fix

--- playwright.config.js
+++ playwright.config.js
@@ -20,10 +20,10 @@ config({
  */
 export default defineConfig({
   //globalSetup : path.resolve('./config/global_setup') ,
-  testDir: './tests',
+  testDir: './tests/TrelloBoard/Templates/*.spec.js',
   /* Run tests in files in parallel */
   fullyParallel: false,
-  testMatch :['/tests/TrelloBoard/Templates'] ,
+  //testMatch :['/tests/TrelloBoard/Templates'] ,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
