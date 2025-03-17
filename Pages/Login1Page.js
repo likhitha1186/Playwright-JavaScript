30f8a30d0b83365dd6da8c94d79cffafb696c5ac
nandu890 <93024230+likhitha1186@users.noreply.github.com>
2025-03-17 18:03:14 +0530

    updated tests

--- Pages/Login1Page.js
+++ Pages/Login1Page.js
@@ -15,9 +15,19 @@ export class Login {
   }
 
   async selectWorkspace(){
-        await this.page.locator("//span[normalize-space()='Workspaces']").click();
-        await this.page.locator("//ul[@data-testid='workspace-switcher-popover']//ul[1]//li[1]//a[1]").click();
+    console.log("Waiting for workspace dropdown...");
+    await this.page.waitForSelector("//span[normalize-space()='Workspaces']", { timeout: 10000 });
+
+    console.log("Workspace dropdown found, clicking...");
+    await this.page.locator("//span[normalize-space()='Workspaces']").click();
+
+    console.log("Waiting for workspace list to appear...");
+    await this.page.waitForSelector("//ul[@data-testid='workspace-switcher-popover']//ul[1]//li[1]//a[1]", { timeout: 10000 });
+
+    console.log("Clicking first workspace...");
+    await this.page.locator("//ul[@data-testid='workspace-switcher-popover']//ul[1]//li[1]//a[1]").click();
   }
+
 
   async selectTemplates(){
         await this.page.getByTestId('templates-menu').click();
