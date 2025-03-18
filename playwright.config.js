7b2a7d4463c9f62728ed78c37ca333e2b5a3ad9a
nandu890 <93024230+likhitha1186@users.noreply.github.com>
2025-03-18 13:37:56 +0530

    updated tests

--- playwright.config.js
+++ playwright.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless:true,
+    headless:false,
     // downloadsPath: `${__dirname}/../downloads`,
     launchOptions: {
       // 1
