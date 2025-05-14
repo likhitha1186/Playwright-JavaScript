a6293f8a26b848854ac89f69dfef339a50a363a1
Likhitha <93024230+likhitha1186@users.noreply.github.com>
2025-05-14 15:33:43 +0530

    Update CreateBoards.spec.js

--- tests/API-TESTING/API-Tests/CreateBoards.spec.js
+++ tests/API-TESTING/API-Tests/CreateBoards.spec.js
@@ -1,15 +1,1 @@
-import {test, expect} from "@playwright/test";
-import {createBoards} from "../HelpersFile/Helpers.js"
 
-const { baseURL, name, apiKey, token } = createBoards();
-
-test('Create Trello Board', async ({ request }) => {
-    const response = await request.post(
-        `${baseURL}/boards/?name=${name}&key=${apiKey}&token=${token}`,
-        {
-            headers: { Accept: 'application/json' },
-        }
-    );
-    console.log(await response.json());
-    expect(response.status()).toBe(200);
-});
