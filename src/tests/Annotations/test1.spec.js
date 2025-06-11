40fa7f127a0045e10c05118a63aca77c10a39799
nandu890 <93024230+likhitha1186@users.noreply.github.com>
2025-06-11 15:57:42 +0530

    updated sone test file

---
+++ src/tests/Annotations/test1.spec.js
@@ -0,0 +1,61 @@
+import { test, expect } from '@playwright/test';
+
+test('Playwright Annotations', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/ ');
+  await page.waitForTimeout(1000);
+
+  await expect(page).toHaveTitle('Swag Labs');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  await page.waitForTimeout(1000);
+});
+
+test.skip('skip() - Annotations', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/ ');
+  await page.waitForTimeout(1000);
+
+  await expect(page).toHaveTitle('Swag Labs');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  await page.waitForTimeout(1000);
+});
+
+test.skip('Annotations', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/ ');
+  await page.waitForTimeout(1000);
+
+  await expect(page).toHaveTitle('Swag Labs');
+
+  await page.fill('#user-name', 'locked_out_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  await page.waitForTimeout(1000);
+
+  await page.click('#add-to-cart-sauce-labs-backpack');
+});
+
+test.fail('fail() - Annotations', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/ ');
+  await page.waitForTimeout(1000);
+
+  await expect(page).toHaveTitle('Swag Labs');
+
+  await page.fill('#user-name', 'locked_out_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  await page.waitForTimeout(1000);
+
+  await expect(page).toHaveTitle('Products');
+});
+
+test("fixme() - Annotations", async ({ page }) => {
+
+})
