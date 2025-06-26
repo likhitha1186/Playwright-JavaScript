1a570ae3416664b880f29550f499d16d94fa6af6
nandu890 <93024230+likhitha1186@users.noreply.github.com>
2025-06-26 15:20:19 +0530

    updated ui-test file

--- src/tests/TrelloBoard/Templates/Page/Login_FilesPage.js
+++ src/tests/TrelloBoard/Templates/Page/Login_FilesPage.js
@@ -9,7 +9,7 @@ export class LoginPage {
     await this.page.goto('https://trello.com/homepage');
     await this.page.setViewportSize({ width: 1920, height: 1040 });
     await this.page.getByText('Log in').click();
-    await this.page.locator('#username').fill(username);
+    await this.page.locator('#username-uid1').fill(username);
     await this.page.locator('button[type="submit"]', { hasText: 'Continue' }).click();
     await this.page.locator('#password').fill(password);
     await this.page.locator('button[type="submit"]', { hasText: 'Log in' }).click();
